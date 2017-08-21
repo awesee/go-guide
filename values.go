d90f1e3d9b1ab814d1fbfc1a7b2aae0c12eb1e0f
Shuo Wang <openset@users.noreply.github.com>
2017-08-21 10:26:42 +0800

    Create values.go

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 连接字符串
+	fmt.Println("go" + "lang")
+	// 计算
+	fmt.Println("1+1=", 1+1, "\n", "string")
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
