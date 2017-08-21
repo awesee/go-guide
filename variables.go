a75b0f9492eac0fb20e46e4447d3cb7db613f92b
Shuo Wang <openset@users.noreply.github.com>
2017-08-21 10:28:34 +0800

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "initial"
+	fmt.Println(a)
+
+	var b, c = "s", 1
+	fmt.Println(b, c)
+
+	var d = false
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
