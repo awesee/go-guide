00de2cd2eae365747c6eaa1ec07b32580c29ca51
Openset <openset.wang@gmail.com>
2017-10-18 17:32:03 +0800

    Add: IsFile

---
+++ php2go/filesystem/is_file.go
@@ -0,0 +1,11 @@
+package filesystem
+
+import "os"
+
+func IsFile(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
