fabe40cfac7e0890285dfe09eeee8d4eed6780b2
Openset <openset.wang@gmail.com>
2017-10-18 15:26:10 +0800

    Add: open-browser

--- Example/open-browser_windows.go
+++ Example/open-browser_windows.go
@@ -3,5 +3,5 @@ package main
 import "os/exec"
 
 func main() {
-	exec.Command("start", "iexplore","http://www.baidu.com").Start()
+	exec.Command("start", "iexplore", "http://www.baidu.com").Start()
 }
