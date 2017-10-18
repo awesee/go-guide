6457a56c122a5324e8851c8651904eb671d03033
Openset <openset.wang@gmail.com>
2017-10-18 16:06:11 +0800

    Add: php echo

---
+++ php2go/echo.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+	//echo "Hello world!";
+	print("Hello world!")
+
+	//echo "Hello world!\n";
+	//echo 'Hello world!', PHP_EOL;
+	println("Hello world!")
+
+	//echo 'This ','string ','was ','made ','with multiple parameters.';
+	print("This ", "string ", "was ", "made ", "with multiple parameters.")
+
+	//echo 'This ','string ','was ','made ','with multiple parameters.', PHP_EOL;
+	println("This ", "string ", "was ", "made ", "with multiple parameters.")
+
+}
---
+++ php2go/print.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	//print "I love Shanghai!";
+	print("I love Shanghai!")
+
+	//print ("I love Shanghai!\n");
+	println("I love Shanghai!")
+
+}
