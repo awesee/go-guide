be9c603ad59f1011bd5dcc1f1aa8986bcb2803f0
Openset <openset.wang@gmail.com>
2018-02-01 17:55:52 +0800

    Update: file_view

--- Example/file_view.go
+++ Example/file_view.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	d := http.Dir("/")
-	fmt.Println(d)
+
 	http.Handle("/", http.FileServer(http.Dir("/")))
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
