ebad68d3cd3048af0a71b78262ff1084221e54ff
Openset <openset.wang@gmail.com>
2018-09-13 16:17:55 +0800

    Add: base64-decimal method 2

--- Example/base64-decimal.go
+++ Example/base64-decimal.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"bytes"
 	"encoding/base64"
-	"encoding/binary"
 	"fmt"
 )
 
 func main() {
 
-	var num rune
+	var num int64
 	fmt.Print("请输入数字：")
 	fmt.Scanf("%d", &num)
-	// fmt.Println("数字: ", num)
 
-	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.BigEndian, num)
-	// fmt.Println("bytes: ", buf.Bytes())
+	// Method 1
+	// buf := new(bytes.Buffer)
+	// binary.Write(buf, binary.BigEndian, num)
+	// bt := bytes.TrimLeftFunc(buf.Bytes(), func(r rune) bool {
+	// 	return r == rune(0)
+	// })
 
-	bt := bytes.TrimLeftFunc(buf.Bytes(), func(r rune) bool {
-		return r == rune(0)
-	})
-	// fmt.Println("bt: ", bt)
-
-	str := base64.StdEncoding.EncodeToString(bt)
+	// Method 2
 	fmt.Println("数字:", num, ", base64编码结果：")
+	bt := []byte{byte(num % 256)}
+	for num >>= 8; num > 0; num >>= 8 {
+		bt = append([]byte{byte(num % 256)}, bt...)
+	}
+	str := base64.StdEncoding.EncodeToString(bt)
 	fmt.Println(str)
 }
